2991ff6d93c68ebbf0f3fb50fb5841c0f2db8424
Jude Nguyen <jude.nguyenchi@gmail.com>
2021-09-01 18:22:43 +0700

    add base activity, view model

---
+++ app/src/main/java/com/example/appmp3/view/base/BaseActivity.java
@@ -0,0 +1,57 @@
+package com.example.appmp3.view.base;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.lifecycle.Observer;
+
+import com.example.appmp3.view.module.dialog.LoadingDialog;
+
+public abstract class BaseActivity<V extends ViewDataBinding, VM extends BaseViewModel> extends AppCompatActivity {
+    private LoadingDialog loadingDialog;
+    private V binding;
+    private VM viewModel;
+
+    protected V getBinding() {
+        return binding;
+    }
+
+    protected VM getViewModel() {
+        return viewModel;
+    }
+
+    private Observer<String> errorObserver = error -> Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+    private Observer<Boolean> loadingObs = new Observer<Boolean>() {
+        @Override
+        public void onChanged(Boolean isShowLoading) {
+            if (isShowLoading) {
+                loadingDialog.show();
+            } else {
+                loadingDialog.dismiss();
+            }
+        }
+    };
+
+    protected abstract int getLayoutId();
+
+    protected abstract VM inflateViewModel();
+
+    protected abstract void init();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, getLayoutId());
+        viewModel = inflateViewModel();
+
+        loadingDialog = new LoadingDialog(this);
+
+        viewModel.errorLiveData.observe(this, errorObserver);
+        viewModel.loadingLiveData.observe(this, loadingObs);
+
+        init();
+    }
+}
--- app/src/main/java/com/example/appmp3/view/module/categorydetail/DetailCategoryActivity.java
+++ app/src/main/java/com/example/appmp3/view/module/categorydetail/DetailCategoryActivity.java
@@ -1,11 +1,7 @@
 package com.example.appmp3.view.module.categorydetail;
 
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,55 +10,40 @@ import com.example.appmp3.R;
 import com.example.appmp3.databinding.ActivityDetailCategoryBinding;
 import com.example.appmp3.model.entity.Category;
 import com.example.appmp3.model.entity.Song;
-import com.example.appmp3.view.module.dialog.LoadingDialog;
+import com.example.appmp3.view.base.BaseActivity;
 import com.example.appmp3.view.module.explorer.adapter.SongAdapter;
 import com.example.appmp3.viewmodel.SongViewModel;
 
 import java.util.List;
 
-public class DetailCategoryActivity extends AppCompatActivity {
+public class DetailCategoryActivity extends BaseActivity<ActivityDetailCategoryBinding, SongViewModel> {
     private SongAdapter songAdapter = new SongAdapter();
     private Category category;
-    private SongViewModel songViewModel;
-    private LoadingDialog loadingDialog;
-
-    private ActivityDetailCategoryBinding activityDetailCategoryBinding;
 
     public static final String EXTRA_CATEGORY = "extra_category";
     private Observer<List<Song>> songListUpdateObserver = songs -> songAdapter.updateAdapter(songs);
-    private Observer<String> errorObserver = error -> Toast.makeText(this, error, Toast.LENGTH_LONG).show();
-    private Observer<Boolean> loadingObs = new Observer<Boolean>() {
-        @Override
-        public void onChanged(Boolean isShowLoading) {
-            if (isShowLoading) {
-                loadingDialog.show();
-            } else {
-                loadingDialog.dismiss();
-            }
-        }
-    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected int getLayoutId() {
+        return R.layout.activity_detail_category;
+    }
 
-        activityDetailCategoryBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail_category);
-        loadingDialog = new LoadingDialog(this);
+    @Override
+    protected SongViewModel inflateViewModel() {
+        return new ViewModelProvider(this).get(SongViewModel.class);
+    }
 
+    @Override
+    protected void init() {
         initRV();
         category = (Category) getIntent().getSerializableExtra(EXTRA_CATEGORY);
-        activityDetailCategoryBinding.setCategory(category);
-        activityDetailCategoryBinding.setDetailCategoryActivity(this);
+        getBinding().setCategory(category);
+        getBinding().setDetailCategoryActivity(this);
 
-        //khoi tao viewmodel
-        songViewModel = new ViewModelProvider(this).get(SongViewModel.class);
         //dang ky lang nghe su kien khi data thay doi
+        getViewModel().songLiveData.observe(this, songListUpdateObserver);
 
-        songViewModel.songLiveData.observe(this, songListUpdateObserver);
-        songViewModel.errorLiveData.observe(this, errorObserver);
-        songViewModel.loadingLiveData.observe(this, loadingObs);
-
-        songViewModel.getSong();
+        getViewModel().getSong();
     }
 
     public void onClickBtnBack(View view) {
@@ -70,7 +51,7 @@ public class DetailCategoryActivity extends AppCompatActivity {
     }
 
     private void initRV() {
-        activityDetailCategoryBinding.recyclerDetailCategory.setAdapter(songAdapter);
-        activityDetailCategoryBinding.recyclerDetailCategory.setLayoutManager(new LinearLayoutManager(this));
+        getBinding().recyclerDetailCategory.setAdapter(songAdapter);
+        getBinding().recyclerDetailCategory.setLayoutManager(new LinearLayoutManager(this));
     }
 }
